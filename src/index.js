966a58fd6741644a0f69d4c6532003b3fb683b91
benson <bensonmunge545@gmail.com>
2025-04-21 16:36:44 +0300

    raect router lab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "login",
+        element: <Login />
+      }
+    ]
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
+);
